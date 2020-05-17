263fdac59f53f04afe6ac0b018fc2edb860b955b
Niranjan Krishnaswamy <nkrishnaswamy@ryerson.ca>
2020-05-17 06:58:08 -0400

    removed buttons from webapp - krishdev sutar

--- WebApp/practice1/src/Counter.js
+++ WebApp/practice1/src/Counter.js
@@ -1,11 +1,9 @@
 import React, {useState}  from 'react';
 import './App.css';
-
 function Counter(){
   const [count1, setCount1] = useState(0);
   const [count2, setCount2] = useState(0);
   const [count3, setCount3] = useState(0);
-
   const add1 = () => {
     setCount1(count1 + 1);
     setCount2(count2 + 1);
@@ -14,7 +12,6 @@ function Counter(){
     setCount3(count3 + 1);
     setCount1(count1 - 1);
   }
-
       return(
         <div className="part2">
           <h1>People Counter</h1>
@@ -24,11 +21,7 @@ function Counter(){
           <h3>{count2}</h3>
           <h3>Number of People Exited</h3>
           <h3>{count3}</h3>
-          <button onClick={add1}>Enter</button>
-          <button onClick={add2}>Exit</button>
-          
         </div>
       );
 }
-
 export default Counter;
