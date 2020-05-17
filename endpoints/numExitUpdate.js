70d9fe904c9fbdc7060374bece1673836fcca258
Niranjan Krishnaswamy <nkrishnaswamy@ryerson.ca>
2020-05-17 04:46:52 -0400

    fixed exitUpdater - endpoint function

--- endpoints/numExitUpdate.js
+++ endpoints/numExitUpdate.js
@@ -1,12 +1,19 @@
 // This function is the webhook's request handler.
 exports = async function(payload) {
+    console.log(payload);
     const mongodb = context.services.get("mongodb-atlas");
     const mycollection = mongodb.db("store_db").collection("numInStore");
     const q = {"docNum": "1"};
+    const body = EJSON.parse(payload.body.text()); 
+    const { numExit } = body;
     const update = {
       "$set": {
-      "numExit": payload.query
+      "numExit": numExit
       }
     };
-    mycollection.updateOne(q, update)
-};
+    return mycollection.updateOne(q, update).then(result => {
+      return {result}
+    }).catch((error) => {
+      return {error};
+    });
+}
