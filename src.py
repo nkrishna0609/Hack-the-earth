e538128ef7d4c537f0087c568ffb1dd8cc1dbf94
rachelwuuu <57509051+rachelwuuu@users.noreply.github.com>
2020-05-17 02:22:14 -0400

    One file is made by me, another file (flask(copied upon muntaser's request).py) is copied on request from Muntaser
    One file is made by me, another file (flask(copied upon muntaser's request).py) is copied on request from Muntaser

---
+++ src.py
@@ -0,0 +1,14 @@
+import serial
+import time
+sig = serial.Serial('COM3',115200,timeout=2)
+print(("COM3",115200))
+
+datafromUser = input()
+if datafromUser =='S':
+    sig.write(b'S')
+    print("yea")
+    time.sleep(200000)
+elif datafromUser == 'O':
+    sig.write(b'O')
+    print("no")
+print("done")
