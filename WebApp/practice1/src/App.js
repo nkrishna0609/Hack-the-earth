6c6014cc39c4f3d6d1b2808ef1dae26521d522d8
Niranjan Krishnaswamy <nkrishnaswamy@ryerson.ca>
2020-05-17 06:36:05 -0400

    uploading webapp on behalf of krishdev sutar

---
+++ WebApp/practice1/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Counter from './Counter';
+// import Hello from './sayHello';
+
+function App(){
+
+  return(
+    <div className="app">
+      <Counter/>
+
+
+
+        {/* <Tweet name="Krish" msg="Hello World"/>
+        <Tweet name="Eren" msg="Hello World"/>
+        <Tweet name="Armin" msg="Hello World"/>
+        <Tweet name="Jean" msg="Hello World"/> */}
+    </div>
+  );
+}
+
+export default App;
