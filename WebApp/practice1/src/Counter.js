30f1353f56206547ad4d6bd39d9d43adfe084b42
KrishdevSutar <43391308+KrishdevSutar@users.noreply.github.com>
2020-05-17 08:50:43 -0400

    Update Counter.js

--- WebApp/practice1/src/Counter.js
+++ WebApp/practice1/src/Counter.js
@@ -1,27 +1,40 @@
-import React, {useState}  from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
-function Counter(){
-  const [count1, setCount1] = useState(0);
-  const [count2, setCount2] = useState(0);
-  const [count3, setCount3] = useState(0);
-  const add1 = () => {
-    setCount1(count1 + 1);
-    setCount2(count2 + 1);
-  }
-  const add2 = () => {
-    setCount3(count3 + 1);
-    setCount1(count1 - 1);
-  }
-      return(
-        <div className="part2">
-          <h1>People Counter</h1>
-          <h3>Number of People in Store</h3>
-          <h3>{count1}</h3>
-          <h3>Number of People Entered</h3>
-          <h3>{count2}</h3>
-          <h3>Number of People Exited</h3>
-          <h3>{count3}</h3>
-        </div>
-      );
+const api =
+    'https://webhooks.mongodb-stitch.com/api/client/v2.0/app/ruhacksapp-uspdy/service/http/incoming_webhook/getter';
+export default class app extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            count1: 0,
+            count2: 0,
+            count3: 0
+        };
+    }
+    componentDidMount() {
+        this.fetchData();
+    }
+
+    fetchData = async () => {
+        const response = await axios.get(api);
+        this.setState({
+            count1: response.data[1]['currentPopulation'],
+            count2: response.data[3]['numEnter'],
+            count3: response.data[0]['numExit']
+        });
+    };
+    render() {
+        return (
+            <div className="part2">
+                <h1>People Counter</h1>
+                <h3>Number of People in Store</h3>
+                <h3>{this.state.count1}</h3>
+                <h3>Number of People Entered</h3>
+                <h3>{this.state.count2}</h3>
+                <h3>Number of People Exited</h3>
+                <h3>{this.state.count3}</h3>
+            </div>
+        );
+    }
 }
-export default Counter;
