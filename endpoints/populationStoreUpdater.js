7e79cdd3fea1ef74ee3c12e55a5682ccadd7d385
Niranjan Krishnaswamy <nkrishnaswamy@ryerson.ca>
2020-05-17 04:49:42 -0400

    fixed populationNum endpoint function

--- endpoints/populationStoreUpdater.js
+++ endpoints/populationStoreUpdater.js
@@ -1,12 +1,19 @@
 // This function is the webhook's request handler.
 exports = async function(payload) {
+    console.log(payload);
     const mongodb = context.services.get("mongodb-atlas");
     const mycollection = mongodb.db("store_db").collection("numInStore");
     const q = {"docNum": "2"};
+    const body = EJSON.parse(payload.body.text()); 
+    const { currentPopulation } = body;
     const update = {
       "$set": {
-      "currentPopulation": payload.query
+      "currentPopulation": currentPopulation
       }
     };
-    mycollection.updateOne(q, update)
-};
+    return mycollection.updateOne(q, update).then(result => {
+      return {result}
+    }).catch((error) => {
+      return {error};
+    });
+}
